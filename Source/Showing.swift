6199601cf3b3aaaf83bb8062abed707cc31ec480
Andre Nguyen <nguyen.andres@gmail.com>
2019-01-29 19:53:27 +0100

    add show options

--- Source/Showing.swift
+++ Source/Showing.swift
@@ -1,21 +1,38 @@
 import UIKit
 
 public func show(whisper message: Message, to: UINavigationController, action: WhisperAction = .show) {
-  whisperFactory.craft(message, navigationController: to, action: action)
+    whisperFactory.craft(message, navigationController: to, action: action)
 }
 
 public func show(shout announcement: Announcement, to: UIViewController, completion: (() -> Void)? = nil) {
-  shoutView.craft(announcement, to: to, completion: completion)
+    shoutView.craft(announcement, to: to, completion: completion)
+}
+
+public func show(whisper message: Message, withColor backgroundColor: UIColor, andTextColor textColor: UIColor, to: UINavigationController, action: WhisperAction = .show) {
+    
+    whisperFactory.craft(message, navigationController: to, action: action)
+    
+    if let _ = whisperFactory.whisperView {
+        whisperFactory.whisperView.backgroundColor = backgroundColor
+        whisperFactory.whisperView.titleLabel.textColor = textColor
+    }
+}
+
+public func show(shout announcement: Announcement, withColor backgroundColor: UIColor, andTextColor textColor: UIColor, to: UIViewController, completion: (() -> Void)? = nil) {
+    shoutView.backgroundView.backgroundColor = backgroundColor
+    shoutView.titleLabel.textColor = textColor
+    shoutView.subtitleLabel.textColor = textColor
+    shoutView.craft(announcement, to: to, completion: completion)
 }
 
 public func show(whistle murmur: Murmur, action: WhistleAction = .show(1.5)) {
-  whistleFactory.whistler(murmur, action: action)
+    whistleFactory.whistler(murmur, action: action)
 }
 
 public func hide(whisperFrom from: UINavigationController, after: TimeInterval = 0) {
-  whisperFactory.silentWhisper(from, after: after)
+    whisperFactory.silentWhisper(from, after: after)
 }
 
 public func hide(whistleAfter after: TimeInterval = 0) {
-  whistleFactory.calm(after: after)
+    whistleFactory.calm(after: after)
 }
